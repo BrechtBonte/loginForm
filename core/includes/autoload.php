3720bcabb49b09f9e25a6b78eb4fe75117c0c812
Hybrided <nbz_tricksta@hotmail.com>
2013-02-14 14:27:58 +0100

    using constant for config.ini

--- core/includes/autoload.php
+++ core/includes/autoload.php
@@ -1,5 +1,5 @@
 <?php
-ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . __DIR__ . '/../../vendor/marlon-be/zend-framework/library');
+ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . APLICATION_PATH . 'core/vendor/marlon-be/zend-framework/library');
 
-$loader = require_once(__DIR__ . '/../../vendor/autoload.php');
-$loader->add('Zend', __DIR__ . '/../../vendor/marlon-be/zend-framework/library');
+$loader = require_once(APLICATION_PATH . 'core/vendor/autoload.php');
+$loader->add('Zend', APLICATION_PATH . 'core/vendor/marlon-be/zend-framework/library');
--- core/includes/require.php
+++ core/includes/require.php
@@ -1,12 +1,14 @@
 <?php
     session_start();
     
+    define('APLICATION_PATH', __DIR__ . '/../../');
+    
     /* AutoLoader */
     require_once('autoload.php');
     
     /* config */
     require_once('Config.php');
-    $config = new Config(__DIR__ . '/../config/config.ini', 'testing');
+    $config = new Zend_Config_Ini(APLICATION_PATH . 'core/config/config.ini', 'testing');
     ini_set('include_path', ini_get('include_path') . PATH_SEPARATOR . $config->path->includes);
     
     require_once('User.php');
